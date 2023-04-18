bf374e8bb6facc95b20e616f1f1626e124f19833
Jayme Martin <jaymelee70@gmail.com>
2023-04-17 19:16:31 -0700

    Final update for module guided project.

--- src/theme/index.js
+++ src/theme/index.js
@@ -1,1 +1,9 @@
 // theme object here
+export default {
+    primaryColor: "royalblue",
+    secondaryColor: "crimson",
+    tertiaryColor: "mediumseagreen",
+    blck: "black",
+    white: "white",
+    breakpointMobile: "(max-width: 550px)"
+}
